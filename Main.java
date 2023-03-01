92fe469c18337a9bb56f31aedd2be41733ede17d
GabrielaCedillo <119831781+GabrielaCedillo@users.noreply.github.com>
2023-03-01 12:27:51 -0600

    Add files via upload
    Gato

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class Main{
+    public static void main (String[] args){
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+    }
+}
+public class Gato{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce la cadena: _XOOXOXO_")
+        String cadena = entrada.nextLine();
+
+    }
+}
