1ff10347e8e120b5fb621c9e7c00b2e7063937df
GabrielaCedillo <119831781+GabrielaCedillo@users.noreply.github.com>
2023-03-27 13:09:55 -0600

    Add files via upload

---
+++ Cafetera.java
@@ -0,0 +1,14 @@
+public class Cafetera {
+    public class Cafetera {
+        private int Deposito_Agua;
+        private int Deposito_Cafe;
+        private int Deposito_Crema;
+        private int Deposito_Vasos;
+
+        public Cafetera() {
+            Deposito_Agua=5000;
+            Deposito_Cafe=1000;
+            Deposito_Crema=1500;
+            Deposito_Vasos=50;
+        }
+}
